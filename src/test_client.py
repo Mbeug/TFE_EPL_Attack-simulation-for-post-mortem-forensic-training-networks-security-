9069e106ba59168780de6b6ee2e5a953abb34b15
Maxime Beugoms <maxime.beugoms@student.uclouvain.be>
2020-05-10 03:22:50 +0200

    add asynchronous attack and add writing output of attack in file in out directory

--- src/test_client.py
+++ src/test_client.py
@@ -1,6 +1,5 @@
 import traceback
-
-
+import threading
 
 from analyser_manager import AnalyserManager
 from attacker_manager import AttackerManager
@@ -9,22 +8,23 @@ from network_manager import NetworkManager
 from simulation_manager import SimulationManager
 from topology_manager import TopologyManager
 from utility import Utility
-from tqdm.auto import tqdm
+
+
 
 class Client:
     """
     this class simulate a client
     """
+    global threads_attack
     if __name__ == "__main__":
-        try :
+        try:
             nm = NetworkManager()
-            tm = TopologyManager(nm,False)
+            tm = TopologyManager(nm, False)
             sim = SimulationManager(nm)
-
+            threads_attack = []
 
             if Utility.ask_user_boolean("Do you want to build the topology?"):
                 # creation of DNS
-                pbar = tqdm(range(9))
                 my_net_config_dns = '''# Static config
                                                        auto eth0
                                                        iface eth0 inet static
@@ -38,21 +38,21 @@ class Client:
                                                            netmask 255.255.255.0'''
 
                 DNS = tm.create_DNS(my_net_config_dns)
-                pbar.update(1)
+                print(ColorOutput.INFO_TAG+": Creating the DNS ...")
                 # Config DNS and DHCP
                 tm.dns_config(DNS)
 
                 # Add some node
+                print(ColorOutput.INFO_TAG+": Creating the workspace area ...")
                 tm.create_n_node(1, "Firefox")
-                pbar.update(2)
                 tm.create_n_node(3, "thomasbeckers/alpine-curl")
-                pbar.update(5)
+                print(ColorOutput.INFO_TAG+": Creating HTTP server ...")
                 tm.create_HTTP()
-                pbar.update(6)
+                print(ColorOutput.INFO_TAG+": Creating FTP server ...")
                 tm.create_FTP()
-                pbar.update(7)
+                print(ColorOutput.INFO_TAG+": Creating DB server")
                 tm.create_db()
-                pbar.update(8)
+
 
                 # config http
                 HTTP = tm.get_http_nodes()[0]
@@ -62,18 +62,21 @@ class Client:
                 FTP = tm.get_ftp_nodes()[0]
                 tm.ftp_config(FTP)
 
+                print(ColorOutput.INFO_TAG+": Creating MAIL server ...")
                 MAIL = tm.create_MAIL()
-                pbar.update(9)
                 db = tm.get_db_nodes()[0]
                 tm.db_config(db)
-                pbar.close()
+
+            print(ColorOutput.INFO_TAG+": Starting nodes ...")
             nm.start_all_nodes()
 
             # Launch simulation phase
+            alpines = None
             if Utility.ask_user_boolean("Do you want to launch the activity simulation?"):
                 alpines = tm.get_pc_nodes("alpine-curl")
+                print("Starting mail activity ...")
                 sim.mail_activity(alpines)
-                sim.kill_mail_activity([alpines[0]])
+                print("Starting FTP activity")
                 sim.http_ftp_activity(alpines)
 
             # Launch attack phase
@@ -81,42 +84,106 @@ class Client:
             if attack_flag:
                 # Add the attacker
                 flag_am_position = ['out', 'in_service', 'in_lan']
-                am = AttackerManager(tm,Utility.ask_user_number("How many attackers do you want?"),Utility.ask_user_choice(flag_am_position, "Where do you want to put it?"))
-
+                position = Utility.ask_user_choice(flag_am_position, "Where do you want to put attackers?")
+                am = AttackerManager(tm, Utility.ask_user_number("How many attackers do you want?"), position)
                 attacker = None
-                if len(am.list_attackers)>0:
+                if len(am.list_attackers) > 0:
                     attacker = am.list_attackers[0]
-                while attack_flag and attacker != None:
+                while attack_flag and attacker is not None:
                     # Ask which attack made
                     attack = Utility.ask_user_choice(['Host discovery', 'Scan ports', 'Dos attack'])
                     if attack == 'Host discovery':
-                        am.host_discovery(attacker,'192.168.122.',0,100)
-                    elif attack == 'Scan ports' :
-                        am.scan_port(attacker,'192.168.122.10',1,1024)
-                    elif attack == 'Dos attack' :
-                        am.dos(attacker,'192.168.122.30','192.168.122.10',80)
+                        if position == flag_am_position[0]:
+                            thread_host = threading.Thread(name="Thread host discovering out", target=am.host_discovery,
+                                             args=[attacker, '192.168.122.', 0, 100])
+                            thread_host.start()
+                            threads_attack.append(thread_host)
+                            #am.host_discovery(attacker, '192.168.122.', 0, 100)
+                        else:
+                            thread_host = threading.Thread(name="Thread host discovering inner", target=am.host_discovery,
+                                                 args=[attacker, '10.0.0.', 0, 100])
+                            thread_host.start()
+                            threads_attack.append(thread_host)
+                            #am.host_discovery(attacker, '10.0.0.', 0, 100)
+                    elif attack == 'Scan ports':
+                        if position == flag_am_position[0]:
+                            thread_scan_port = threading.Thread(name="Thread Scan port out", target=am.scan_port,
+                                                 args=[attacker, '192.168.122.10', 1, 1024])
+                            thread_scan_port.start()
+                            threads_attack.append(thread_scan_port)
+                            #am.scan_port(attacker, '192.168.122.10', 1, 1024)
+                        else:
+                            thread_scan_port = threading.Thread(name="Thread Scan port inner", target=am.scan_port,
+                                                 args=[attacker, '192.168.122.10', 1, 1024])
+                            thread_scan_port.start()
+                            threads_attack.append(thread_scan_port)
+                            am.scan_port(attacker, '192.168.122.10', 1, 1024)
+                    elif attack == 'Dos attack':
+                        if position == flag_am_position[0]:
+                            thread_dos = threading.Thread(name="Thread Dos attack out", target=am.dos,
+                                                 args=[attacker, '192.168.122.30', '192.168.122.10', 80])
+                            thread_dos.start()
+                            threads_attack.append(thread_dos)
+                            #am.dos(attacker, '192.168.122.30', '192.168.122.10', 80)
+                        else:
+                            thread_dos = threading.Thread(name="Thread Dos attack in", target=am.dos,
+                                                 args=[attacker, '192.168.122.30', '192.168.122.10', 80])
+                            thread_dos.start()
+                            threads_attack.append(thread_dos)
+                            #am.dos(attacker, '192.168.122.30', '192.168.122.10', 80)
 
-                    attack_flag=Utility.ask_user_boolean("Do you want to make an other attack?")
-
+                    attack_flag = Utility.ask_user_boolean("Do you want to make an other attack?")
 
             # Launch the analyse phase
             if Utility.ask_user_boolean("Do you want to make an analyse?"):
                 anm = AnalyserManager(nm)
                 anm.start_nodes()
-                anm.stop_nodes()
-                anm.start(DNS)
-                anm.stop(DNS)
-                anm.start_all_capture()
-                anm.stop_all_capture()
-                anm.start_capture_btw(DNS, HTTP)
-                anm.stop_capture_btw(DNS, HTTP)
+                if Utility.ask_user_boolean("Do you want to do overall capture?"):
+                    anm.start_all_capture()
+                    anm.stop_all_capture()
+                analyse_flag = Utility.ask_user_boolean("Do you want to do a specific capture")
+                while analyse_flag:
+                    choices = ['DNS', 'HTTP', 'FTP', 'MAIL']
+                    analyse = Utility.ask_user_choice(choices,"Which node do you want to capture?")
+                    if analyse == 'DNS':
+                        anm.start_capture_node(DNS)
+                        anm.stop_capture_node(DNS)
+                    elif analyse == 'HTTP':
+                        anm.start_capture_node(HTTP)
+                        anm.stop_capture_node(HTTP)
+                    elif analyse == 'FTP':
+                        anm.start_capture_node(FTP)
+                        anm.stop_capture_node(FTP)
+                    elif analyse == 'MAIL':
+                        anm.start_capture_node(MAIL)
+                        anm.stop_capture_node(MAIL)
 
-        except Exception :
+                    analyse_flag = Utility.ask_user_boolean("Do you want to do an other?")
+                anm.stop_nodes()
+
+
+            # Stop the activity simulation
+            if alpines is not None:
+                print("Stopping activity ...")
+                nm.start_all_nodes()
+                sim.kill_mail_activity(alpines)
+                sim.kill_http_ftp_activity(alpines)
+                nm.stop_all_node()
+
+
+
+        except Exception:
             print(ColorOutput.ERROR_TAG)
             traceback.print_exc()
 
         finally:
             # end of client
-            if Utility.ask_user_boolean("Do you want to close all?"):
-                nm.stop_all_node()
-                tm.clean()
+            print(ColorOutput.INFO_TAG+": waiting threads for attacks processes ...")
+            for thread in threads_attack:
+                thread.join()
+
+            nm.stop_all_node()
+
+            if nm is not None and tm is not None:
+                if Utility.ask_user_boolean("Do you want to close all?"):
+                    tm.clean()
