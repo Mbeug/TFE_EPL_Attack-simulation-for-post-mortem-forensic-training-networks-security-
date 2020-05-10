94119bb0a556344ff848015902acd80e9d35bd99
Thomas Beckers <thomas.beckers@student.uclouvain.be>
2020-05-10 19:23:51 +0200

    Add doc

--- src/docker_manager.py
+++ src/docker_manager.py
@@ -8,7 +8,16 @@ import docker
 class DockerManager:
     """
     This class discuss with dockers
+
+    Example::
+        from docker_manager import DockerManager
+        dm = DockerManager('vm')
+        cont_name = dm.select_container()
+        dm.copy_to_docker('./python_scripts/write_file.py',cont_name)
+        res = dm.exec_to_docker(cont_name, 'python3 write_file.py new_file.txt')
+        print(res.output)
     """
+
     def __init__(self, selected_machine):
         # Use the default socket
         if selected_machine == 'local':
@@ -17,6 +26,11 @@ class DockerManager:
             self.client = docker.DockerClient(base_url = 'tcp://192.168.56.104:2375')
 
     def select_container(self):
+        """
+        Prompt a selection of running container
+
+        :return: short_id of the selected container
+        """
         count = 0
         for container in self.client.containers.list():
             print("["+str(count)+"]", end = ' ')
@@ -27,7 +41,16 @@ class DockerManager:
         return self.client.containers.list()[selected_container].short_id
 
     def copy_to_docker(self, src, cont_name, dst='/'):
-        #TODO resolve not copying files to docker
+        """
+        Copy a file or folder to a specific docker container
+
+        :param src: path of the file to copy
+        :param cont_name: name of the container (for exemple short_id)
+        :param dst:  destination path in the container
+        :type src: str
+        :type dst: str
+        :return: True if the copy was successful
+        """
         container = self.client.containers.get(cont_name)
 
         cur_dir = os.getcwd()
@@ -48,16 +71,17 @@ class DockerManager:
         return res
 
     def exec_to_docker(self, cont_name, cmd, isdetach=False)->Tuple[int,bytes]:
+        """
+        Execute a command inside a container
+
+        :param cont_name: name of the container (for exemple short_id)
+        :param cmd: command to execute
+        :param isdetach: True if we want to detach
+        :type cont_name: str
+        :type src: str
+        :type dst: bool
+        :return: tuple with the exit code and the output of the command
+        """
         container = self.client.containers.get(cont_name)
         res = container.exec_run(cmd,detach=isdetach)
-        # print(res)
         return res
-
-# Example
-#from docker_manager import DockerManager
-# dm = DockerManager('vm')
-# cont_name = dm.select_container()
-# dm.copy_to_docker('./python_scripts/write_file.py',cont_name)
-# res = dm.exec_to_docker(cont_name, 'python3 write_file.py new_file.txt')
-# print(res.output)
-
--- src/simulation_manager.py
+++ src/simulation_manager.py
@@ -15,6 +15,22 @@ class SimulationManager:
         pass
 
     def mail_activity(self, list_pc, t_min = 2, t_max = 5, sender = "bob", passwd = "alice", receiver = "bob"):
+        """
+        Start mail activity on machines with curl
+
+        :param list_pc: list of targeted machines 
+        :param t_min: min time between each request
+        :param t_max: max time between each request
+        :param sender: sender of mails
+        :param passwd: password of the sender
+        :param receiver: recipient of the mail
+        :type list_pc: list
+        :type t_min: int
+        :type t_max: int
+        :type sender: str
+        :type passwd: str
+        :type receiver: str
+        """
         for pc in list_pc:
             container = pc["properties"]["container_id"]
             self.dm.copy_to_docker("./config_files/client/requests_mail.sh", container)
@@ -24,12 +40,30 @@ class SimulationManager:
         pass
 
     def kill_mail_activity(self, list_pc):
+        """
+        Stop mail activity on machines
+
+        :param list_pc: list of targeted machines
+        :type list_pc: list
+        """
         for pc in list_pc:
             container = pc["properties"]["container_id"]
             self.dm.exec_to_docker(container, "ash kill_mail.sh")
         pass
 
     def http_ftp_activity(self, list_pc, t_min = 2, t_max = 5, path_url = "./config_files/client/"):
+        """
+        Start http/ftp activity on machines with curl
+
+        :param list_pc: list of targeted machines 
+        :param t_min: min time between each request
+        :param t_max: max time between each request
+        :param path_url: path to the file containing urls
+        :type list_pc: list
+        :type t_min: int
+        :type t_max: int
+        :type path_url: str
+        """
         for pc in list_pc:
             container = pc["properties"]["container_id"]
             self.dm.copy_to_docker("./config_files/client/requests_urls.sh", container)
@@ -39,6 +73,12 @@ class SimulationManager:
         pass
 
     def kill_http_ftp_activity(self, list_pc):
+        """
+        Stop http/ftp activity on machines with curl
+
+        :param list_pc: list of targeted machines
+        :type list_pc: list
+        """
         for pc in list_pc:
             container = pc["properties"]["container_id"]
             self.dm.exec_to_docker(container, "ash kill_urls.sh")
